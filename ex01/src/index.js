875be52b55d5440c6dc8ba6f0c1175c4537dc208
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-10-04 00:11:32 +0200

    Added a few tweaks !

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,26 +1,24 @@
-    'use strict';
-    var i, c;
+var i, c;
     
-    function init() {
-        i=document.getElementById('keyboard').getElementsByTagName('button');
-        for(c=0; c < i.length; c++) {
-            if(i[c].type === 'button') {
-                i[c].addEventListener('onclick', makeClickHandler(c));
-            }
+function init() {
+    i = document.getElementById('keyboard').getElementsByTagName('button');
+    for(c = 0; c < i.length; c++) {
+        if(i[c].type === 'button') {
+            i[c].addEventListener('onclick', makeClickHandler(c));
         }
     }
+}
  
-    function makeClickHandler(c) {
-        i[c].onclick=function() {
-            document.getElementById('textArea').value += this.value.toLowerCase();
-        }
+function makeClickHandler(c) {
+    i[c].onclick = function() {
+        document.getElementById('textArea').value += this.value.toLowerCase();
     }
+}
 
-    function backSpace() {
-        var string = document.getElementById('textArea').value;
-        document.getElementById('textArea').value = string.substring(0, string.length - 1);
-    }
- 
-    window.addEventListener?
-    window.addEventListener('load', init, false):
-    window.attachEvent('onload', init);
+/* var backSpace = function() {
+    var string = document.getElementById('textArea').value;
+    document.getElementById('textArea').value = string.substring(0, string.length - 1);
+} */
+
+window.addEventListener?
+window.addEventListener('load', init, false) : window.attachEvent('onload', init);
